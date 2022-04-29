afd674bcabcb889c86c996ea09f1ff65ec9e5e4b
damccull <401104+damccull@users.noreply.github.com>
2022-04-29 15:50:24 -0400

    Make fields public

--- src/models/datastore/peer_state.rs
+++ src/models/datastore/peer_state.rs
@@ -4,10 +4,10 @@ use crate::models::p2p::PeerAddress;
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct PeerState {
-    address: PeerAddress,
-    blacklist_timestamp: Option<u64>, // If None, not blacklisted, else, time blacklist was issued
-    brs_version: Option<String>,
-    last_contact: Option<u64>, // unix timestamp or perhaps timestamp from signum epoch
-    total_bytes_downloaded_lifetime: u64,
-    total_bytes_uploaded_lifetime: u64,
+    pub address: PeerAddress,
+    pub blacklist_timestamp: Option<u64>, // If None, not blacklisted, else, time blacklist was issued
+    pub brs_version: Option<String>,
+    pub last_contact: Option<u64>, // unix timestamp or perhaps timestamp from signum epoch
+    pub total_bytes_downloaded_lifetime: u64,
+    pub total_bytes_uploaded_lifetime: u64,
 }
