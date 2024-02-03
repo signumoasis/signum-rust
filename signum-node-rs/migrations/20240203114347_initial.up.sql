c0d4657d87bfe2b0cc26fbe89ece175deee4f078
damccull <401104+damccull@users.noreply.github.com>
2024-02-03 11:29:04 +0000

    Add initial migration

---
+++ signum-node-rs/migrations/20240203114347_initial.up.sql
@@ -0,0 +1,9 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS peers (
+    peer_address TEXT PRIMARY KEY,
+    application TEXT,
+    version TEXT,
+    platform TEXT,
+    share_address BOOLEAN,
+    network TEXT
+);
