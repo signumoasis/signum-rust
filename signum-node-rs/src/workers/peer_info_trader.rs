2619f68130054d27bd12a2fc542642e784b82c64
damccull <401104+damccull@users.noreply.github.com>
2024-02-06 09:40:56 +0000

    Stub out basic logic for peer info trader

--- signum-node-rs/src/workers/peer_info_trader.rs
+++ signum-node-rs/src/workers/peer_info_trader.rs
@@ -1,7 +1,32 @@
+use std::time::Duration;
+
 use anyhow::Result;
 
+use crate::models::p2p::{PeerAddress, PeerInfo};
+
+pub async fn run_peer_info_trader_forever() -> Result<()> {
+    loop {
+        let result = peer_info_trader().await;
+        if result.is_err() {
+            tracing::error!("Error in peer info trader: {:?}", result);
+        }
+        tokio::time::sleep(Duration::from_secs(60)).await;
+    }
+}
 /// Gets info from peer nodes and stores it.
 /// Simultaneously supplies this node's info to the peers it contacts.
 pub async fn peer_info_trader() -> Result<()> {
+    // Get all peers from the database that haven't been seen in 1 minute
+    let peers = Vec::<PeerInfo>::new();
+    // Loop through the list to attempt to get the info for each one
+    for peer in peers {
+        // Spawn update info task
+        tokio::spawn(update_info_task(peer));
+    }
+
+    Ok(())
+}
+
+pub async fn update_info_task(peer: PeerInfo) -> Result<PeerInfo> {
     todo!()
 }
