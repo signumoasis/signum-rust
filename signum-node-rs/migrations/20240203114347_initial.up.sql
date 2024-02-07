03a180656c6f4dc3060df0c7e995a31cbd0555f5
damccull <401104+damccull@users.noreply.github.com>
2024-02-07 08:04:52 +0000

    Make peer_ip_address nullable

--- signum-node-rs/migrations/20240203114347_initial.up.sql
+++ signum-node-rs/migrations/20240203114347_initial.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS peers (
     peer_announced_address TEXT PRIMARY KEY NOT NULL,
-    peer_ip_address TEXT NOT NULL UNIQUE,
+    peer_ip_address TEXT UNIQUE,
     application TEXT,
     version TEXT,
     platform TEXT,
