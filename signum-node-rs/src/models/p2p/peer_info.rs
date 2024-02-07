72f5c9a8ce433a5a6b262a10a8b30652ae55e4e4
damccull <401104+damccull@users.noreply.github.com>
2024-02-07 07:13:03 +0000

    Make announcedAddress required

--- signum-node-rs/src/models/p2p/peer_info.rs
+++ signum-node-rs/src/models/p2p/peer_info.rs
@@ -5,7 +5,7 @@ use super::PeerAddress;
 #[derive(Debug, Default, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PeerInfo {
-    pub announced_address: Option<PeerAddress>,
+    pub announced_address: PeerAddress,
     pub application: String,
     pub version: String,
     pub platform: Option<String>,
