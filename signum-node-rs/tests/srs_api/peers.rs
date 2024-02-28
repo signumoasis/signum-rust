ac5ffac0d902b3469204e7886fd6a4197fc0b670
damccull <401104+damccull@users.noreply.github.com>
2024-02-27 23:11:36 -1000

    Add basic peers test. See if it responds with 200.

--- signum-node-rs/tests/srs_api/peers.rs
+++ signum-node-rs/tests/srs_api/peers.rs
@@ -1,1 +1,20 @@
+use crate::helpers::spawn_app;
 
+#[tokio::test]
+async fn srs_api_handler_works() {
+    // Arrange
+    let app = spawn_app().await;
+
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .post(&format!("{}/signum", &app.address))
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
