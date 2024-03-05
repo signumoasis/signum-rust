2a5f8033534998294f98d5a7b3c6a02eaec4b50c
damccull <401104+damccull@users.noreply.github.com>
2024-03-04 16:20:55 -1000

    Add test for getPeers request

--- signum-node-rs/tests/srs_api/peers.rs
+++ signum-node-rs/tests/srs_api/peers.rs
@@ -23,3 +23,27 @@ async fn srs_api_handler_returns_valid_data_for_get_info_request() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[tokio::test]
+async fn srs_api_handler_returns_valid_data_for_get_peers_request() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let body = serde_json::json!({
+        "requestType": "getPeers",
+    });
+
+    // Act
+    let response = client
+        .post(&format!("{}/", &app.address))
+        .header("Content-Type", "application/json")
+        .json(&body)
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_ne!(Some(0), response.content_length());
+}
