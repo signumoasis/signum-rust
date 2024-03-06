809c90edc4dcae07545a0c7c223b9e9e912a4b0b
damccull <401104+damccull@users.noreply.github.com>
2024-03-05 16:03:03 -1000

    Set up test to compare to a template

--- signum-node-rs/tests/srs_api/peers.rs
+++ signum-node-rs/tests/srs_api/peers.rs
@@ -1,9 +1,10 @@
+use anyhow::Context;
 use signum_node_rs::srs_api::request_models::GetInfoRequestModel;
 
 use crate::helpers::spawn_app;
 
 #[tokio::test]
-async fn srs_api_handler_returns_valid_data_for_get_info_request() {
+async fn srs_api_handler_returns_valid_data_for_get_info_request() -> Result<(), anyhow::Error> {
     // Arrange
     let app = spawn_app().await;
 
@@ -30,9 +31,22 @@ async fn srs_api_handler_returns_valid_data_for_get_info_request() {
     // Assert
     assert!(response.status().is_success());
     assert_ne!(Some(0), response.content_length());
-    let json = response.json::<GetInfoRequestModel>().await;
+    let json = response
+        .json::<GetInfoRequestModel>()
+        .await
+        .context("couldn't deserialize json")?;
     //TODO: Remove this and do an actual test on test data, probably input from TestApp
     println!("JSON IS: {:#?}", json);
+    let template = GetInfoRequestModel {
+        announced_address: Some(format!("http://localhost:{}", app.port)),
+        application: Some("SignumRust".to_string()),
+        version: Some("0.1.0".to_string()),
+        platform: Some("Test".to_string()),
+        share_address: Some(false),
+        network_name: "TEST".to_string(),
+    };
+    assert_eq!(json, template);
+    Ok(())
 }
 
 #[tokio::test]
