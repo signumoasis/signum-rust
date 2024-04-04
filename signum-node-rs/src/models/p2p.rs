a0770d0319714900ac6f6fb19b87090d53aa3e6b
damccull <401104+damccull@users.noreply.github.com>
2024-04-03 22:16:05 -1000

    Add two api models for block and transaction

--- signum-node-rs/src/models/p2p.rs
+++ signum-node-rs/src/models/p2p.rs
@@ -1,11 +1,14 @@
 mod block_id;
 mod exchangeable_block;
+mod exchangeable_transaction;
 mod peer_address;
 mod peer_info;
 mod transaction;
 
 pub use block_id::BlockId;
 pub use exchangeable_block::ExchangeableBlock;
+pub use exchangeable_transaction::ExchangeableTransaction;
+pub use exchangeable_transaction::ExchangeableTransactionAttachment;
 pub use peer_address::PeerAddress;
 pub use peer_info::PeerInfo;
 pub use transaction::Transaction;
--- signum-node-rs/src/models/p2p/exchangeable_block.rs
+++ signum-node-rs/src/models/p2p/exchangeable_block.rs
@@ -1,2 +1,23 @@
-#[derive(Debug)]
-pub struct ExchangeableBlock;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ExchangeableBlock {
+    version: u8,
+    timestamp: String,
+    previous_block: u64,
+    total_amount_nqt: u64,
+    total_fee_nqt: u64,
+    total_fee_cashback_nqt: u64,
+    total_fee_burnt_nqt: u64,
+    payload_length: u32,
+    payload_hash: String,
+    generator_public_key: String,
+    generation_signature: String,
+    /// `previous_block_hash` is only valid in v1 blocks.
+    previous_block_hash: Option<String>,
+    block_signature: String,
+    transactions: Vec<ExchangeableTransaction>,
+    nonce: u64,
+    base_target: u64,
+    block_ats: String,
+}
