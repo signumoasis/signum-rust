08150bd564350764104a918873b0f085ad9d323b
damccull <401104+damccull@users.noreply.github.com>
2024-04-05 17:05:21 -1000

    Rename ExchangeableBlock to B1Block and  add serde renames

--- signum-node-rs/src/models/p2p.rs
+++ signum-node-rs/src/models/p2p.rs
@@ -1,12 +1,12 @@
+mod b1_block;
 mod block_id;
-mod exchangeable_block;
 mod exchangeable_transaction;
 mod peer_address;
 mod peer_info;
 mod transaction;
 
+pub use b1_block::B1Block;
 pub use block_id::BlockId;
-pub use exchangeable_block::ExchangeableBlock;
 pub use exchangeable_transaction::ExchangeableTransaction;
 pub use exchangeable_transaction::ExchangeableTransactionAttachment;
 pub use peer_address::PeerAddress;
--- signum-node-rs/src/models/p2p/b1_block.rs
+++ signum-node-rs/src/models/p2p/b1_block.rs
@@ -2,13 +2,18 @@ use crate::models::p2p::ExchangeableTransaction;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct ExchangeableBlock {
+#[serde(rename_all = "camelCase")]
+pub struct B1Block {
     version: u8,
     timestamp: String,
     previous_block: u64,
+    #[serde(rename = "totalAmountNQT")]
     total_amount_nqt: u64,
+    #[serde(rename = "totalFeeNQT")]
     total_fee_nqt: u64,
+    #[serde(rename = "totalFeeCashbasckNQT")]
     total_fee_cashback_nqt: u64,
+    #[serde(rename = "totalFeeBurntNQT")]
     total_fee_burnt_nqt: u64,
     payload_length: u32,
     payload_hash: String,
@@ -20,5 +25,6 @@ pub struct ExchangeableBlock {
     transactions: Vec<ExchangeableTransaction>,
     nonce: u64,
     base_target: u64,
+    #[serde(rename = "blockATs")]
     block_ats: String,
 }
