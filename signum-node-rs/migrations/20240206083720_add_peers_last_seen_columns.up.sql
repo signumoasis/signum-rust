21197f4fc5e7098e1bd5a758d78520d0cc179fb9
damccull <401104+damccull@users.noreply.github.com>
2024-04-05 21:09:16 -1000

    Remove unnecessary files.

--- signum-node-rs/migrations/20240206083720_add_peers_last_seen_columns.up.sql
+++
@@ -1,6 +0,0 @@
--- Add up migration script here
-ALTER TABLE peers
-ADD COLUMN last_seen DATETIME;
-
-ALTER TABLE peers
-ADD COLUMN attempts_since_last_seen INTEGER NOT NULL DEFAULT 0;
--- signum-node-rs/sql/create_peer_in_db.sql
+++
@@ -1,8 +0,0 @@
--- SQLite
-BEGIN TRANSACTION;
-INSERT INTO peers (peer_address, application, version, share_address, network)
-VALUES ("p2p.signumoasis.xyz:80", "SignumRust", "0.1.0", false, "Signum");
-INSERT INTO peers (peer_address, application, version, share_address, network)
-VALUES ("us-east.signum.network:8123", "BRS", "3.8.0", false, "Signum");
-COMMIT;
-select * from peers;
