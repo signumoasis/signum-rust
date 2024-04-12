134ad5c7fe74e4db0bad5fd521c76d3b3614805b
damccull <401104+damccull@users.noreply.github.com>
2024-04-12 10:06:15 -0600

    Continue work on block downloader

--- signum-node-rs/src/workers/block_downloader.rs
+++ signum-node-rs/src/workers/block_downloader.rs
@@ -1,9 +1,16 @@
 use anyhow::Result;
-use std::time::Duration;
+use serde_json::json;
+use std::{collections::VecDeque, time::Duration};
 use tracing::Instrument;
 use uuid::Uuid;
 
-use crate::{configuration::Settings, models::datastore::Datastore};
+use crate::{
+    configuration::Settings,
+    models::{
+        datastore::Datastore,
+        p2p::{B1Block, PeerAddress},
+    },
+};
 
 pub async fn run_block_downloader_forever(database: Datastore, settings: Settings) -> Result<()> {
     loop {
@@ -19,18 +26,66 @@ pub async fn run_block_downloader_forever(database: Datastore, settings: Setting
         if result.is_err() {
             tracing::error!("Error in block downloader: {:?}", result);
         }
-        tokio::time::sleep(Duration::from_secs(5)).await;
+        tokio::time::sleep(Duration::from_secs(60)).await;
     }
 }
 
-/// This worker finds new peers by querying the existing peers in the database.
-/// If no peers exist in the database, it will read from the configuration bootstrap
-/// peers list.
+/// This worker queries random peers for new blocks.
 #[tracing::instrument(name = "Block Downloader", skip_all)]
-pub async fn block_downloader(_database: Datastore, _settings: Settings) -> Result<()> {
-    tracing::info!("Downloading a block");
-    // let client = reqwest::Client::new();
+pub async fn block_downloader(mut database: Datastore, _settings: Settings) -> Result<()> {
+    tracing::info!("Would download blocks");
+    // Steps:
+    // * Initiate FIFO queue
+    // * Determine highest block stored in DB
+    // * Determine the mode of the highest cumulative difficulty
+    //   of several or maybe all known peers
+    // * Build a list of peers with the discovered cumulative difficulty
     //
-    // let response = client.post();
+    // NOTES:
+    // Things to keep track of:
+    // * Which block sets have been queried - handled by FIFO task queue
+    // * Which ones have errored - handled by FIFO task queue, but must be re-requested
+    //
+    let _downloads = VecDeque::<B1Block>::new();
+
+    let _highest_cumulative_difficulty = 0u128;
+    let peers = database.get_n_random_peers(5).await?;
+
+    tracing::debug!("Random peers from db: {:#?}", &peers);
+
+    let _cumulative_difficulties: Vec<u128> = Vec::new();
+
+    for _peer in peers {}
+
+    Ok(())
+}
+
+async fn download_blocks_task(peer: PeerAddress, height: u64, number_of_blocks: u64) -> Result<()> {
+    let thebody = json!({
+        "protocol": "B1",
+        "requestType": "getBlocksFromHeight",
+        "height": height,
+        "numBlocks": number_of_blocks,
+    });
+    // let mut thebody = HashMap::new();
+    // thebody.insert("protocol", "B1");
+    // thebody.insert("requestType", "getBlocksFromHeight");
+    // thebody.insert("height", height);
+
+    let peer_request = reqwest::Client::new()
+        .post(peer.to_url())
+        .header("User-Agent", "BRS/3.8.0")
+        .json(&thebody)
+        .send()
+        .await?;
+
+    tracing::trace!("Parsing peers...");
+    #[derive(Debug, serde::Deserialize)]
+    struct PeerContainer {
+        #[serde(rename = "peers")]
+        peers: Vec<PeerAddress>,
+    }
+    let response = peer_request.json::<PeerContainer>().await?;
+    tracing::trace!("Peers successfully parsed: {:#?}", &response);
     Ok(())
 }
