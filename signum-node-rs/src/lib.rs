215c556dde3958746f5eb0cf5f742780e05302bc
damccull <401104+damccull@users.noreply.github.com>
2024-05-21 21:29:44 -1000

    Add generic method to get the mode of some collection

--- signum-node-rs/src/lib.rs
+++ signum-node-rs/src/lib.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use actix_web::HttpResponse;
+use itertools::Itertools;
 
 pub mod configuration;
 pub mod models;
@@ -23,4 +26,49 @@ pub fn error_chain_fmt(
 #[tracing::instrument(skip_all)]
 pub async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(skip(input))]
+pub fn statistics_mode<T>(input: impl IntoIterator<Item = T>) -> Option<T>
+where
+    T: std::fmt::Debug + std::hash::Hash + std::cmp::Eq,
+{
+    let frequencies = input.into_iter().fold(HashMap::new(), |mut freqs, value| {
+        *freqs.entry(value).or_insert(0) += 1;
+        freqs
+    });
+
+    let mut frequencies = frequencies.into_iter().max_set_by_key(|(_, count)| *count);
+    if frequencies.len() > 1 || frequencies.is_empty() {
+        return None;
+    };
+
+    let (thing, _) = frequencies.remove(0);
+
+    Some(thing)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::statistics_mode;
+
+    #[test]
+    fn statistics_mode_returns_some_max_value() {
+        let test_data = vec![4, 1, 1, 2, 4, 3, 4, 5];
+
+        let result = statistics_mode(test_data);
+        assert_eq!(result, Some(4));
+    }
+
+    #[test]
+    fn statistics_mode_returns_none_if_no_mode() {
+        let test_data_no_repeats = vec![1, 2, 3, 4, 5];
+        let test_data_multiple_options = vec![1, 1, 2, 2, 3, 4, 5];
+
+        let result = statistics_mode(test_data_no_repeats);
+        assert_eq!(result, None, "Failed on no repeats");
+
+        let result = statistics_mode(test_data_multiple_options);
+        assert_eq!(result, None, "Failed on multiple options");
+    }
 }
