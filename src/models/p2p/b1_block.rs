3dab06055f087f82f2369759d26c4146f1a760f6
damccull <401104+damccull@users.noreply.github.com>
2024-08-05 15:58:20 -1000

    Refactor to deserialize json better

--- src/models/p2p/b1_block.rs
+++ src/models/p2p/b1_block.rs
@@ -1,30 +1,53 @@
 use crate::models::p2p::B1Transaction;
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
 
+#[serde_as]
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct B1Block {
     version: u8,
-    timestamp: String,
+
+    /// Some number since Signum's start epoch
+    //#[serde_as(as = "DisplayFromStr")]
+    timestamp: u64,
+
+    #[serde_as(as = "DisplayFromStr")]
     previous_block: u64,
+
     #[serde(rename = "totalAmountNQT")]
     total_amount_nqt: u64,
+
     #[serde(rename = "totalFeeNQT")]
     total_fee_nqt: u64,
-    #[serde(rename = "totalFeeCashbasckNQT")]
+
+    #[serde(rename = "totalFeeCashBackNQT")]
     total_fee_cashback_nqt: u64,
+
     #[serde(rename = "totalFeeBurntNQT")]
     total_fee_burnt_nqt: u64,
+
     payload_length: u32,
+
     payload_hash: String,
+
     generator_public_key: String,
+
     generation_signature: String,
+
     /// `previous_block_hash` is only valid in v1 blocks.
     previous_block_hash: Option<String>,
+
     block_signature: String,
+
     transactions: Vec<B1Transaction>,
+
+    #[serde_as(as = "DisplayFromStr")]
     nonce: u64,
+
+    #[serde_as(as = "DisplayFromStr")]
     base_target: u64,
+
     #[serde(rename = "blockATs")]
     block_ats: Option<String>,
 }
