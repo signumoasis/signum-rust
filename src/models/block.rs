bffa2af2aa32e126474b2b36e7b6864231c9e4eb
damccull <401104+damccull@users.noreply.github.com>
2024-09-16 16:05:30 -1000

    Stub out some block conversion code

--- src/models/block.rs
+++ src/models/block.rs
@@ -1,2 +1,18 @@
 #[derive(Debug)]
 pub struct Block;
+
+impl From<ExchangeableBlock> for Block {
+    fn from(value: ExchangeableBlock) -> Self {
+        todo!()
+    }
+}
+
+/// Maybe not needed at all
+#[derive(Debug)]
+pub struct ExchangeableBlock;
+
+impl From<Block> for ExchangeableBlock {
+    fn from(value: Block) -> Self {
+        todo!()
+    }
+}
